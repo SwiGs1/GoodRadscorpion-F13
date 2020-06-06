14c8dba9180757ba1a156af20e25e08cefb49267
Afya212 <54920261+Afya212@users.noreply.github.com>
2020-06-06 17:59:39 -0400

    Revert "Travis"

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
 source dependencies.sh
 
-source ~/.nvm/nvm.sh
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-npm install --global yarn
-
-pip3 install --user PyYaml
-pip3 install --user beautifulsoup4
-
-phpenv global $PHP_VERSION
+if [ "$BUILD_TOOLS" = true ]; then
+	  source ~/.nvm/nvm.sh
+	  nvm install $NODE_VERSION
+      pip3 install --user PyYaml
+      pip3 install --user beautifulsoup4
+fi;
